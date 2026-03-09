56b269906542930f1bbc68fbcbc507018d0cbd1d
asolermaria <asolermaria@gmail.com>
2026-03-09 13:32:35 +0100

    Done

--- scripts.js
+++ scripts.js
@@ -1,1 +1,33 @@
-console.log(document.title);
+// Ejercicio 1
+document.getElementById("gen-1").innerText = "Generasión 1 Pokimon"
+
+//Ejercicio 2
+let infocard = document.querySelectorAll("#gen-1 + div .infocard")
+for (const pokemon of infocard) {
+  pokemon.style.backgroundColor = "red"
+}
+
+// Ejercicio 3
+console.log(window.location.href);
+
+//Ejercicio 4
+console.log(document.domain);
+
+// Ejercicio 5
+const images = document.querySelectorAll(".img-sprite");
+console.log(images);
+
+//Ejercicio 6
+const todasLasImagenes = document.querySelectorAll("img");
+
+todasLasImagenes.forEach((img) => {
+  img.src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+});
+
+// Ejercicio 7
+const flying = document.querySelectorAll(".flying")
+
+for (const fly of flying) {
+  fly.style.backgroundColor = 'black'
+  
+}
